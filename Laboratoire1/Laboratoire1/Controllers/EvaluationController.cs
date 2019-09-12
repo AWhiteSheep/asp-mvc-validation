fb2ba20d830d065b804cceb4c51d6623894c1b46
awhitesheep <yanharc@gmail.com>
2019-09-12 08:06:07 -0400

    Added structure to the project

--- Laboratoire1/Laboratoire1/Controllers/EvaluationController.cs
+++ Laboratoire1/Laboratoire1/Controllers/EvaluationController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Laboratoire1.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace Laboratoire1.Controllers
 {
@@ -10,7 +12,7 @@ namespace Laboratoire1.Controllers
     {
         public RedirectToActionResult Index()
         {
-            return RedirectToAction("Accueil");
+            return RedirectToAction(nameof(Accueil));
         }
 
         public IActionResult Accueil()
@@ -21,6 +23,42 @@ namespace Laboratoire1.Controllers
         public IActionResult List()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Create(Evaluation evaluation)
+        {
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpGet]
+        public IActionResult Update(int num_evaluation)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Update(IFormCollection form)
+        {
+            return RedirectToAction(nameof(List));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int num_evaluation)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Delete(int num_evaluation, Evaluation evaluation)
+        {
+            return RedirectToAction(nameof(List));
         }
     }
 }
