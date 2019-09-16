6882bcec547c802de744ae994ac1aaa03d76b63f
awhitesheep <yanharrc@gmail.com>
2019-09-16 18:17:37 -0400

    TagHelpers for ease of use

---
+++ Laboratoire1/src/Laboratoire1/TagHelpers/RowcolminTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratoire1.TagHelpers
+{
+    public class RowcolminTagHelper : TagHelper
+    {
+        public string aspColLength { get; set; }
+        /// <summary>
+        /// Si aucun est spécifié il ne sera pas lié avec une colonne pour la valeur
+        /// </summary>
+        public string aspId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "row");
+            // avant la conception de l'élément html il ajoute un div qui est la colonne
+            var _colLength = aspColLength != null ? "col-" + aspColLength : "col";
+            output.PreContent.AppendHtml($@"<div class='{_colLength}'>");
+            // après la conception de l'élément html il ajoute un div qui lui agira comme cible pour le jquery
+            // pour avoir un modal dynamique si le id est donné
+            var _postHtml = aspId != null ? $"</div><div class='{_colLength}'><div id='{aspId}'></div></div>" : $"</div>";
+            output.PostContent.AppendHtml(_postHtml);
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
---
+++ Laboratoire1/src/Laboratoire1/TagHelpers/RowevaluationTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Laboratoire1.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratoire1.TagHelpers
+{
+    public class RowevaluationTagHelper : TagHelper
+    {
+        public Evaluation eval { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div"; // change the tag to a section
+            output.Attributes.SetAttribute("class", "row");
+            output.Attributes.SetAttribute("data-evaluation-numero", eval.num_evaluation);
+            output.Attributes.SetAttribute("data-evaluation-nom", eval.nom_eleve);
+            output.Attributes.SetAttribute("data-evaluation-prenom", eval.prenom_eleve);
+            output.Attributes.SetAttribute("data-evaluation-telephone", eval.telephone_eleve);
+            output.Attributes.SetAttribute("data-evaluation-courriel", eval.courriel_eleve);
+            output.Attributes.SetAttribute("data-evaluation-genre", eval.genre_eleve);
+            output.Attributes.SetAttribute("data-evaluation-note", eval.note_evaluation);
+            output.Attributes.SetAttribute("data-evaluation-date", eval.date_evaluation);
+            output.Attributes.SetAttribute("data-evaluation-commentaire", eval.commentaires_evaluation);
+            output.Attributes.SetAttribute("onclick", "etudiantShow(this)");
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
