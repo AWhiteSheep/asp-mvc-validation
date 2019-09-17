60cb4598dfe1263148f1adad2cf00ab77dde10d6
awhitesheep <yanharc@gmail.com>
2019-09-17 09:40:32 -0400

    Validation côté serveur

--- Laboratoire1/src/Laboratoire1/Models/Evaluation.cs
+++ Laboratoire1/src/Laboratoire1/Models/Evaluation.cs
@@ -18,27 +18,28 @@ namespace Laboratoire1.Models
         [RegularExpression(@"^[0-9]+$")]
         public int num_evaluation { get; set; }
         [Required]
-        [Display(Name = "Étudiant")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Display(Name = "Nom de l'étudiant")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$", ErrorMessage = "Le prénom  doit commencer par une majuscule.")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Le champ doit être au minimum {0} charactères.")]
         public string nom_eleve { get; set; }
         [Required]
         [Display(Name = "Prénom")]
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$", ErrorMessage = "Le prénom  doit commencer par une majuscule.")]
         public string prenom_eleve { get; set; }
         [Required]
         [Display(Name = "Téléphone étudiant")]
         public string telephone_eleve { get; set; }
         [Required]
         [Display(Name = "Courriel étudiant")]
+        [DataType(DataType.EmailAddress)]
         public string courriel_eleve { get; set; }
         [Required]
         [Display(Name = "Sexe étudiant")]
-        [RegularExpression("^H|h|f|F$")]
+        [RegularExpression("^H|h|f|F$", ErrorMessage = "Le genre ne peut qu'être H, h, f ou F")]
         public char genre_eleve { get; set; }
         [Required]
         [Display(Name = "Note évaluation")]
-        [RegularExpression(@"^[0-9]+$")]
-        [Range(0, 100)]
+        [Range(0, 100 , ErrorMessage = "La note doît être de 0 à 100")]
         public string note_evaluation { get; set; }
         [Required]
         [Display(Name = "Date évaluation")]
