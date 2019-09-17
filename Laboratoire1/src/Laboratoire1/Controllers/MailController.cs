539a5c91690728d67264bf6eeebb05362faf1098
awhitesheep <yanharc@gmail.com>
2019-09-17 02:30:22 -0400

    MailController

---
+++ Laboratoire1/src/Laboratoire1/Controllers/MailController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratoire1.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Send()
+        {
+            return View();
+        }
+        [HttpPost]
+        public RedirectToRouteResult Send(FormCollection form)
+        {
+            return RedirectToRoute("Evaluation/Index");
+        }
+
+    }
+}
