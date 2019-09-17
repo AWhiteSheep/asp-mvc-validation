6ae34574fdde293c3b5a5d63f19d41cb311cebd6
awhitesheep <yanharc@gmail.com>
2019-09-17 14:08:06 -0400

    Add email object and cshtml

---
+++ Laboratoire1/src/Laboratoire1/Models/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratoire1.Models
+{
+    public class Email
+    {
+        [Required]
+        public string from { get; set; }
+        [Required]
+        public string to { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string subject { get; set; }
+        [Required]
+        public string content { get; set; }
+    }
+}
